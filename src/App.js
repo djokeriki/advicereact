4604ba8d0bc4fa9f78bc749e90cbf11d4c808a73
djokeriki <risteminovski@gmail.com>
2020-09-06 18:24:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import Main from './components/Main';
-import Wrapper from './components/Wrapper';
+import React, {useEffect, useState} from 'react'
+import Recipe from './Recipe';
+import {v1 as uuid} from 'uuid';
+import './App.css'
 
 function App() {
+  const APP_ID = 'f1e0674a';
+  const APP_KEY = 'b81f460cf3e1ffbdc7c8c628d4d8d6bd';
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+  useEffect(() => {
+    getRecipes();
+  }, [query])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
   return (
-    
-    <Wrapper>
-      <Main />
-    </Wrapper>
-    
-  );
+    <div>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">Search</button>
+        
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe => (
+        <Recipe 
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image} 
+        ingredients={recipe.recipe.ingredients}
+        key={uuid()} />
+      ))}
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-   <div className="header">
-       <h1 className="header-title">Forecast Finder</h1>
-   </div> 
-)
-
-export default Header;
